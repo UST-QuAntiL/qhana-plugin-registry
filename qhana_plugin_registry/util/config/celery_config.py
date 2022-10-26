e25ed9f3ff3f9834ac4278ce9153ccf395e900dc
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2022-10-26 16:29:24 +0200

    Change default celery queue

--- qhana_plugin_registry/util/config/celery_config.py
+++ qhana_plugin_registry/util/config/celery_config.py
@@ -14,7 +14,7 @@
 
 from collections import ChainMap
 
-CELERY_PRODUCTION_CONFIG = {}
+CELERY_PRODUCTION_CONFIG = {"task_default_queue": "qhana_plugin_registry"}
 
 CELERY_DEBUG_CONFIG = ChainMap(
     {
