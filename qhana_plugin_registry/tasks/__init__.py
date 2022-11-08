6135f12b2c4f904feac0c0c063b776b6cbf5e8fc
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2022-11-08 16:50:00 +0100

    Add purging of plugins that are  not available
    Plugins that were not available for a configurable duration arepurged.
    Time is counted from the latest time any plugin was available.

--- qhana_plugin_registry/tasks/__init__.py
+++ qhana_plugin_registry/tasks/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from celery import Celery
 
-from .plugin_discovery import start_plugin_discovery
+from .plugin_discovery import start_plugin_discovery, purge_plugins
 
 
 def register_periodic_tasks(app: Flask, celery: Celery):
@@ -12,6 +12,7 @@ def register_periodic_tasks(app: Flask, celery: Celery):
         celery (Celery): celery instance to register tasks for
     """
     _register_plugin_discovery_task(app, celery)
+    _register_plugin_purge_task(app, celery)
 
 
 def _register_plugin_discovery_task(app: Flask, celery: Celery):
@@ -40,4 +41,37 @@ def _register_plugin_discovery_task(app: Flask, celery: Celery):
         plugin_discovery_intervall,
         start_plugin_discovery.s(),
         name="Plugin discovery",
+    )
+
+
+def _register_plugin_purge_task(app: Flask, celery: Celery):
+    """Register the plugin purge periodic task with celery.
+
+    Args:
+        app (Flask): flask app passed for configuration values
+        celery (Celery): celery instance to register tasks for
+    """
+    plugin_purge_intervall: int = app.config.get("PLUGIN_PURGE_INTERVAL", 15 * 60)
+
+    if not isinstance(plugin_purge_intervall, (int, float)):
+        raise TypeError(
+            "The plugin purge intervall setting has the wrong type! (expected int or float)"
+        )
+
+    if plugin_purge_intervall == -1:
+        return  # -1 prevents recurring task entirely
+
+    if plugin_purge_intervall < 5:
+        raise ValueError(
+            f"The shortest allowed intervall for the plugin purging task is 5 seconds (got {plugin_purge_intervall})."
+        )
+
+    purge_after = app.config.get("PLUGIN_PURGE_AFTER", "never")
+    if purge_after in ("never", -1) or purge_after is None:
+        return  # plugins will not be purged so there is no need to schedule the task
+
+    celery.add_periodic_task(
+        plugin_purge_intervall,
+        purge_plugins.s(),
+        name="Plugin purge",
     )
