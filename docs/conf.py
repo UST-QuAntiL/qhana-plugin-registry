3f74ff0396b6c656fa99374c6fa6a7d3c8d31de8
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2022-12-11 22:21:22 +0100

    Fix OpenAPI rendering in docs

--- docs/conf.py
+++ docs/conf.py
@@ -213,6 +213,8 @@ redoc = [
     },
 ]
 
+redoc_uri = "https://unpkg.com/redoc@latest/bundles/redoc.standalone.js"
+
 # myst markdown parsing
 _myst_options = sphinx_config.get("myst", {})
 allowed_md_extensions = {
